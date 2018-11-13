e7abf21eaa3625c41e9a8226ce01285fccc606f7
Stoffer <robin.stoffer@wur.nl>
2018-11-13 17:34:30 +0100

    Improved plots: added grid points.

--- Training data/test_downsampling.py
+++ Training data/test_downsampling.py
@@ -15,21 +15,27 @@ import sys
 test = finegrid['output']['w']['variable'][finegrid.kgc_edge:finegrid.khend, finegrid.jgc:finegrid.jend, finegrid.igc:finegrid.iend]
 test2 = np.mean(test,axis = -1)
 test3 = np.mean(test2,axis = -1)
+test4 = test3.copy()
 z = finegrid['grid']['z'][finegrid.kgc_center:finegrid.kend]
 z = np.insert(z, 0, 0.0)
 z = np.append(z, finegrid['grid']['zsize'])
 test3 = np.insert(test3, 0, test3[0])
 plt.figure()
 plt.step(z, test3, label = 'fine')
+z_grid = finegrid['grid']['zh'][finegrid.kgc_edge:finegrid.khend]
+plt.plot(z_grid, test4,'C0o')
 
 test = coarsegrid['output']['w']['variable'][coarsegrid.kgc_edge:coarsegrid.khend, coarsegrid.jgc:coarsegrid.jend, coarsegrid.igc:coarsegrid.iend]
 test2 = np.mean(test,axis = -1)
 test3 = np.mean(test2,axis = -1)
+test4 = test3.copy()
 z = coarsegrid['grid']['z'][coarsegrid.kgc_center:coarsegrid.kend]
 z = np.insert(z, 0, 0.0)
 z = np.append(z, coarsegrid['grid']['zsize'])
 test3 = np.insert(test3, 0, test3[0])
 plt.step(z, test3, label = 'coarse')
+z_grid = coarsegrid['grid']['zh'][coarsegrid.kgc_edge:coarsegrid.khend]
+plt.plot(z_grid, test4,'C1o')
 
 plt.legend()
 
@@ -38,17 +44,23 @@ test = finegrid['output']['p']['variable'][finegrid.kgc_center:finegrid.kend, fi
 test2 = np.mean(test,axis = -1)
 test3 = np.mean(test2,axis = -1)
 #test3 = np.append(test3, test3[-1])
+test4 = test3.copy()
 test3 = np.insert(test3, 0, test3[0])
 z = finegrid['grid']['zh'][finegrid.kgc_edge:finegrid.khend]
 plt.figure()
 plt.step(z, test3, label = 'fine')
+z_grid = finegrid['grid']['z'][finegrid.kgc_center:finegrid.kend]
+plt.plot(z_grid, test4,'C0o')
 
 test = coarsegrid['output']['p']['variable'][coarsegrid.kgc_center:coarsegrid.kend, coarsegrid.jgc:coarsegrid.jend, coarsegrid.igc:coarsegrid.iend]
 test2 = np.mean(test,axis = -1)
 test3 = np.mean(test2,axis = -1)
+test4 = test3.copy()
 test3 = np.insert(test3, 0, test3[0])
 #test3 = np.append(test3, test3[-1])
 z = coarsegrid['grid']['zh'][coarsegrid.kgc_edge:coarsegrid.khend]
 plt.step(z, test3, label = 'coarse')
+z_grid = coarsegrid['grid']['z'][coarsegrid.kgc_center:coarsegrid.kend]
+plt.plot(z_grid, test4,'C1o')
 
 plt.legend()
