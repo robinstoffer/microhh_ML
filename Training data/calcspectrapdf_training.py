48dd52df3129ec74398e464b45202c877cf22840
Robin Stoffer <robin.stoffer@wur.nl>
2019-11-05 17:44:45 +0100

    Add script to visualize horizontal crosssections of velocity fields with spectra and pdfs.

--- Training data/calcspectrapdf_training.py
+++ Training data/calcspectrapdf_training.py
@@ -1,126 +1,69 @@
 import sys
 import numpy
 import struct
-import netCDF4
+import netCDF4 as nc
 #import pdb
 #import tkinter
 import matplotlib as mpl
 mpl.use('agg') #Prevent that Matplotlib uses Tk, which is not configured for the Python version I am using
 from matplotlib.pyplot import *
-sys.path.append("/home/robin/microhh2/python")
-from microhh_tools import *
 
-#nx = 768
-#ny = 384
-#nz = 256
+#Define height indices to consider the xy-crosssection. When comparing the plots from this script to the plots of the equivalent script for the horizontal crosssections produced by MicroHH, ensure that these height indices are consistent!!!
+indexes_local = [0,1,2,3,16,32,47,60,61,62,63]
 
-nx = 96
-ny = 48
-nz = 64
 
-#iter = 60000
-#iterstep = 500
-#nt   = 7
+#Fetch training data
+training_filepath = "/projects/1/flowsim/simulation1/lesscoarse/training_data.nc"
+a = nc.Dataset(training_filepath, 'r')
 
-iter = 0000
+#Read friction velocity
+ustar   = np.array(a['utau_ref'][:], dtype = 'f4')
 
-# read the grid data
-n = nx*ny*nz
+#Read ghost cells, indices, and coordinates from the training file
+igc        = int(a['igc'][:])
+jgc        = int(a['jgc'][:])
+kgc_center = int(a['kgc_center'][:])
+kgc_edge   = int(a['kgc_edge'][:])
+iend       = int(a['iend'][:])
+jend       = int(a['jend'][:])
+kend       = int(a['kend'][:])
+zhc        = np.array(a['zhc'][:])
+zc         = np.array(a['zc'][:])
+yhc        = np.array(a['yhc'][:])
+yc         = np.array(a['yc'][:])
+xhc        = np.array(a['xhc'][:])
+xc         = np.array(a['xc'][:])
 
-fin = open("/home/robin/microhh2/cases/moser600lesNN_restart/grid.{:07d}".format(0),"rb")
-raw = fin.read(nx*8)
-x   = numpy.array(struct.unpack('<{}d'.format(nx), raw))
-raw = fin.read(nx*8)
-xh  = numpy.array(struct.unpack('<{}d'.format(nx), raw))
-raw = fin.read(ny*8)
-y   = numpy.array(struct.unpack('<{}d'.format(ny), raw))
-raw = fin.read(ny*8)
-yh  = numpy.array(struct.unpack('<{}d'.format(ny), raw))
-raw = fin.read(nz*8)
-z   = numpy.array(struct.unpack('<{}d'.format(nz), raw))
-raw = fin.read(nz*8)
-zh  = numpy.array(struct.unpack('<{}d'.format(nz), raw))
-fin.close()
+nz = zc.shape[0]
+ny = yc.shape[0]
+nx = xc.shape[0]
 
-# read the 3d data and process it
-print("Processing iter = {:07d}".format(iter))
+#Read variables from netCDF-file for an arbitrarty time step t=0
+#NOTE: undo normalisation with friction velocity!
+t=0 #Can be any time step, results should not change substantially
+uc_singlefield = np.array(a['uc'][t,kgc_center:kend,jgc:jend,igc:iend]) * ustar
+vc_singlefield = np.array(a['vc'][t,kgc_center:kend,jgc:jend,igc:iend]) * ustar
+wc_singlefield = np.array(a['wc'][t,kgc_center:kend,jgc:jend,igc:iend]) * ustar
 
-fin = open("/home/robin/microhh2/cases/moser600lesNN_restart/u.{:07d}".format(iter),"rb")
-raw = fin.read(n*8)
-tmp = numpy.array(struct.unpack('<{}d'.format(n), raw))
-del(raw)
-u   = tmp.reshape((nz, ny, nx))
-del(tmp)
-fin.close()
-
-uavg = numpy.nanmean(numpy.nanmean(u,2),1)
-
-fin = open("/home/robin/microhh2/cases/moser600lesNN_restart/v.{:07d}".format(iter),"rb")
-raw = fin.read(n*8)
-tmp = numpy.array(struct.unpack('<{}d'.format(n), raw))
-del(raw)
-v   = tmp.reshape((nz, ny, nx))
-del(tmp)
-fin.close()
-
-vavg = numpy.nanmean(numpy.nanmean(v,2),1)
-
-utotavg = (uavg**2. + vavg**2.)**.5
-visc    = 1.0e-5
-ustar   = (visc * utotavg[0] / z[0])**0.5
-
-print('u_tau  = %.6f' % ustar)
-print('Re_tau = %.2f' % (ustar / visc))
-
-#Define height data
-yplus  = z  * ustar / visc
-yplush = zh * ustar / visc
-
-starty = 0
-endy   = int(z.size / 2)
-
-# read cross-sections
-variables=["u","v","w"]
+#Loop over heights to calculate spectra
+num_var = 3
 nwave_modes_x = int(nx * 0.5)
 nwave_modes_y = int(ny * 0.5)
 spectra_x  = numpy.zeros((3,nz,nwave_modes_x))
 spectra_y  = numpy.zeros((3,nz,nwave_modes_y))
 pdf_fields = numpy.zeros((3,nz,ny,nx))
 index_spectra = 0
-input_dir = '/home/robin/microhh2/cases/moser600lesNN_restart/'
-for crossname in variables:
-    
-    if(crossname == 'u'): loc = [1,0,0]
-    elif(crossname=='v'): loc = [0,1,0]
-    elif(crossname=='w'): loc = [0,0,1]
-    else:                 loc = [0,0,0]
-    
-    locx = 'x' if loc[0] == 0 else 'xh'
-    locy = 'y' if loc[1] == 0 else 'yh'
-    locz = 'z' if loc[2] == 0 else 'zh'
-    
-    indexes_local = get_cross_indices(crossname, 'xy')
+for idx_var in range(num_var):
     stop = False
     for k in range(np.size(indexes_local)):
         index = indexes_local[k]
-        zplus = yplus if locz=='z' else yplush
-        f_in  = "{0:}.xy.{1:05d}.{2:07d}".format(crossname, index, iter)
-        try:
-            fin = open(input_dir + f_in, "rb")
-        except:
-            print('Stopping: cannot find file {}'.format(f_in))
-            crossfile.sync()
-            stop = True
-            break
-        
-        print("Processing %8s, time=%7i, index=%4i"%(crossname, iter, index))
-        
-        #fin = open("{0:}.xy.{1:05d}.{2:07d}".format(crossname, index, prociter), "rb")
-        raw = fin.read(nx*ny*8)
-        tmp = np.array(st.unpack('{0}{1}d'.format("<", nx*ny), raw))
-        del(raw)
-        s = tmp.reshape((ny, nx))
-        del(tmp)
+        if idx_var == 0:
+            s = uc_singlefield[index,:,:]
+
+        elif idx_var == 1:
+            s = vc_singlefield[index,:,:]  
+        else:
+            s = wc_singlefield[index,:,:]
         fftx = numpy.fft.rfft(s,axis=1)*(1/nx)
         ffty = numpy.fft.rfft(s,axis=0)*(1/ny)
         Px = fftx[:,1:] * numpy.conjugate(fftx[:,1:])
@@ -138,7 +81,6 @@ for crossname in variables:
         spectra_x[index_spectra,k,:]    = numpy.nanmean(Ex,axis=0) #Average Fourier transform over the direction where it was not calculated
         spectra_y[index_spectra,k,:]    = numpy.nanmean(Ey,axis=1)
         pdf_fields[index_spectra,k,:,:] = s[:,:]
-        fin.close()
 
     index_spectra +=1
 
@@ -152,7 +94,6 @@ bin_edges_v = np.linspace(np.nanmin(pdf_fields[1,:,:]),np.nanmax(pdf_fields[1,:,
 bin_edges_w = np.linspace(np.nanmin(pdf_fields[2,:,:]),np.nanmax(pdf_fields[2,:,:]), num_bins)
 
 #Plot balances
-indexes_local = get_cross_indices('u', 'xy')
 for k in range(np.size(indexes_local)):
     figure()
     loglog(k_streamwise[:], (spectra_x[0,k,:] / ustar**2.), 'k-',linewidth=2.0, label='u')
@@ -167,7 +108,7 @@ for k in range(np.size(indexes_local)):
     grid()
     axis([1, 250, 0.000001, 3])
     tight_layout()
-    savefig("/home/robin/microhh2/cases/moser600lesNN_restart/spectrax_z_" + str(indexes_local[k]) + ".png")
+    savefig("/home/robinst/microhh/cases/moser600/git_repository/Training data/spectrax_z_" + str(indexes_local[k]) + "_training.png")
     close()
     #
     figure()
@@ -183,7 +124,7 @@ for k in range(np.size(indexes_local)):
     grid()
     axis([1, 250, 0.000001, 3])
     tight_layout()
-    savefig("/home/robin/microhh2/cases/moser600lesNN_restart/spectray_z_" + str(indexes_local[k]) + ".png")
+    savefig("/home/robinst/microhh/cases/moser600/git_repository/Training data/spectray_z_" + str(indexes_local[k]) + "_training.png")
     close()
     #
     figure()
@@ -197,7 +138,7 @@ for k in range(np.size(indexes_local)):
     grid()
     axis([0, 0.16, 0, 140])
     tight_layout()
-    savefig("/home/robin/microhh2/cases/moser600lesNN_restart/pdfu_z_" + str(indexes_local[k]) + ".png")
+    savefig("/home/robinst/microhh/cases/moser600/git_repository/Training data/pdfu_z_" + str(indexes_local[k]) + "_training.png")
     close()
     #
     figure()
@@ -210,7 +151,7 @@ for k in range(np.size(indexes_local)):
     yticks(fontsize = 16, rotation = 0)
     axis([-0.04, 0.04, 0, 140])
     tight_layout()
-    savefig("/home/robin/microhh2/cases/moser600lesNN_restart/pdfv_z_" + str(indexes_local[k]) + ".png")
+    savefig("/home/robinst/microhh/cases/moser600/git_repository/Training data/pdfv_z_" + str(indexes_local[k]) + "_training.png")
     close()
     #
     figure()
@@ -223,6 +164,9 @@ for k in range(np.size(indexes_local)):
     yticks(fontsize = 16, rotation = 0)
     axis([-0.03, 0.03, 0, 140])
     tight_layout()
-    savefig("/home/robin/microhh2/cases/moser600lesNN_restart/pdfw_z_" + str(indexes_local[k]) + ".png")
+    savefig("/home/robinst/microhh/cases/moser600/git_repository/Training data/pdfw_z_" + str(indexes_local[k]) + "_training.png")
     close()
     #
+
+#Close nc-file
+a.close()
